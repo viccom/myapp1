20d7dd86657b09fa2b8fe4d232d4353df7897619
e <3520845@qq.com>
2017-03-22 16:02:23 +0800

    modify myapp1_api

---
+++ myapp1/myapp1_api.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+from __future__ import unicode_literals
+import frappe
+import json
+import requests
+from frappe import throw, msgprint, _
+from frappe.model.document import Document
+from frappe.utils import cint
+
+@frappe.whitelist(allow_guest=True)
+def test():
+	r = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+	if r:
+		return r.json();
